6f453231ea525345372be5eddf32368f782d31c4
RajChak93 <r.chakraborty@unmc.edu>
2025-05-16 22:48:59 -0700

    Delete run_everywhere_except_apple.py

--- run_everywhere_except_apple.py
+++
@@ -1,6 +0,0 @@
-with open('resources/targets_without_istam.txt', 'rt') as logfile:
-    for line in logfile:
-        deployment = line.strip()
-        print(
-            f'METASTORE_TYPE=PROD VAULT_TYPE=PROD SCRIPT_PATH=jun5_deployment/v5.sql DEPLOYMENT={deployment} SECRETS_PROVIDER=VAULT java -jar snowhouse-accountUsageViewGenerator-jar-with-dependencies.jar')
-
